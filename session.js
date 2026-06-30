3f176a09676989069821082083c03d3598742a03
video-yt <videoyt823@gmail.com>
2026-06-30 19:37:08 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1779971597112_gxfyahh10",
 PORT: process.env.PORT || "8000"
 };
